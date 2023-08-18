f0fc9508b0da5fb52a2f0131e838c997bfd048fe
Joshvictor69 <142469138+Joshvictor69@users.noreply.github.com>
2023-08-18 01:18:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 // Define the list of songs with their file paths
 const songs = [
-    { title: 'Song 1', path: 'https://drive.google.com/uc?export=download&id=1VE-XF-DAp5GO3VBND8YRkQvfsmfM7QVH' },
-    { title: 'Song 2', path: 'https://drive.google.com/uc?export=download&id=1qP28-A7MJa7e22MV-Nc7DXLWeUfvc4z7' }
+    { title: 'Cant Unsuck', path: 'https://drive.google.com/uc?export=download&id=1VE-XF-DAp5GO3VBND8YRkQvfsmfM7QVH' },
+    { title: 'Infinity Jam', path: 'https://drive.google.com/uc?export=download&id=1qP28-A7MJa7e22MV-Nc7DXLWeUfvc4z7' },
+     { title: 'The Slip', path: 'https://drive.google.com/uc?export=download&id=1nlegdVcFyK_qRcHCnXkQx53vb8H1ZLWM' },
+    { title: 'Laloo - Live', path: 'https://drive.google.com/uc?export=download&id=1nlegdVcFyK_qRcHCnXkQx53vb8H1ZLWM' },
+    { title: 'Smoka The Wood', path: 'https://drive.google.com/uc?export=download&id=1jgNXOfl1ZX70vsQdl0RANK468iZ-aq-8' }
 ];
 // Get the song list element and the audio player element
 const songList = document.getElementById('song-list');
